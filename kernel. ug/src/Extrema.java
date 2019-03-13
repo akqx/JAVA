98be60d7d761f97c2f0d19218c4c00da4b389b61
akqx <kurelagnieszka@gmail.com>
2019-03-13 15:01:55 +0100

    change extremas

--- kernel. ug/src/Extrema.java
+++ kernel. ug/src/Extrema.java
@@ -39,24 +39,32 @@ public class Extrema {
 		List<Point> minima = new ArrayList<>();
 		List<Point> maxima = new ArrayList<>();
 		Point previous = points.get(points.size() - 1);
-		for (int i = 1; i < points.size(); i++) {
+		for (int i = 1; i < points.size() - 1; i++) {
 
 			Point p = points.get(i - 1);
 			Point next = points.get(i);
+			Point nextnext = points.get(i + 1);
 			// wyznaczone warunki:
 			// https://inf.ug.edu.pl/~lmielewczyk/teaching/ns/graf-geo-alg/lab/lab1.php
 			if (p.getY() > previous.getY() && p.getY() > next.getY() && !PointsHelper.isClockwise(previous, p, next)
-					|| p.getY() > previous.getY() && p.getY() == next.getY()
+
+					|| p.getY() > previous.getY() && p.getY() == next.getY() && next.getY() > nextnext.getY()
 							&& !PointsHelper.isClockwise(previous, p, next))
 				maxima.add(p);
-			if (p.getY() < previous.getY() && p.getY() < next.getY() && !PointsHelper.isClockwise(previous, p, next))
+
+			if (p.getY() < previous.getY() && p.getY() < next.getY() && !PointsHelper.isClockwise(previous, p, next)
+					|| p.getY() < previous.getY() && p.getY() == next.getY() && next.getX() < nextnext.getY()
+							&& !PointsHelper.isClockwise(previous, p, next))
 				minima.add(p);
 
 			previous = p;
 		}
 
 		Point min = getMinimum(minima, maxY(points));
+		System.out.println(min);
+
 		Point max = getMaximum(maxima, minY(points));
+		System.out.println(max);
 
 		return new Kernel(points, min, max);
 	}
